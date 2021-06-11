e9fb14f944668e2cb5b2327f34a0b756dad5f809
amirhnajafiz <najafizadeh21@gmail.com>
2021-06-11 16:08:34 +0430

    Router routes set basics

--- Ring-Frontend/src/router/index.js
+++ Ring-Frontend/src/router/index.js
@@ -2,6 +2,12 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Login from '@/components/Login-Register/Login'
+import Register from '@/components/Login-Register/Register'
+import Dashboard from '@/components/Game/Dashboard'
+import Factory from '@/components/Game/Factory'
+import Land from '@/components/Game/Land'
+import Market from '@/components/Game/Market'
+import AdminDash from '@/components/Admin/AdminDash'
 
 // Initializing vue-router
 Vue.use(Router)
@@ -11,8 +17,38 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Login',
+      name: 'login',
       component: Login
+    },
+    {
+      path: '/Register',
+      name: 'register',
+      component: Register
+    },
+    {
+      path: '/Dashboard',
+      name: 'dashboard',
+      component: Dashboard
+    },
+    {
+      path: '/Factory',
+      name: 'factory',
+      component: Factory
+    },
+    {
+      path: '/Land',
+      name: 'land',
+      component: Land
+    },
+    {
+      path: '/Market',
+      name: 'market',
+      component: Market
+    },
+    {
+      path: '/AdminDash',
+      name: 'admindash',
+      component: AdminDash
     }
   ]
 })
