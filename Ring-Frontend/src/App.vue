e55a1b21c4a4ca0c805e3b48e1067b4b7faf7b6a
amirhnajafiz <najafizadeh21@gmail.com>
2021-06-12 10:40:37 +0430

    Login page layout set, form handling next plan

--- Ring-Frontend/src/App.vue
+++ Ring-Frontend/src/App.vue
@@ -55,9 +55,7 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  height: 100vh;
 }
 
 .navigation-for-test {
--- Ring-Frontend/src/components/Login-Register/Login.vue
+++ Ring-Frontend/src/components/Login-Register/Login.vue
@@ -1,13 +1,43 @@
 <template>
-  <div>
-    <h4>Please Login</h4>
-    <form action="" method="GET">
-      <label for="username">User name</label>
-      <input id="username" type="text" v-model="username" required autofocus />
-      <label for="password">Password</label>
-      <input id="password" type="password" v-model="password" required />
-      <input type="submit" value="Login" />
-    </form>
+  <div class="h-100 blue-login-background">
+    <div class="container h-80">
+      <div class="row align-items-center h-100">
+        <div class="col-3 mx-auto">
+          <div class="text-center">
+            <p class="profile-name-card">Please Login into website</p>
+            <form
+              class="form-signin"
+              id="login-form"
+              @submit.prevent="sendLogInForm"
+              method="GET"
+            >
+              <input
+                id="username"
+                type="text"
+                v-model="username"
+                class="form-control form-group mt-3"
+                placeholder="User Name"
+                required
+                autofocus
+              />
+              <input
+                id="password"
+                type="password"
+                v-model="password"
+                placeholder="Password"
+                class="form-control form-group mt-3"
+                required
+              />
+              <input
+                class="btn btn-lg btn-primary btn-block btn-signin mt-3 subbtn"
+                type="submit"
+                value="Login"
+              />
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -20,5 +50,31 @@ export default {
       password: "",
     };
   },
+  methods: {
+    sendLogInForm() {
+      console.log(this.username + " " + this.password);
+    },
+  },
 };
 </script>
+
+<style>
+.h-80 {
+  height: 80vh;
+}
+
+.blue-login-background {
+  background-image: linear-gradient(to bottom right, #000066, #ffffff);
+}
+
+.subbtn {
+  background: #000066;
+  color: #ffffff;
+  border-color: #000066;
+}
+
+.subbtn:hover {
+  background: #ffffff;
+  color: #000066;
+}
+</style>
