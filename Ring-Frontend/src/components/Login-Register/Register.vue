26b570e9e6a56d4bf6b2298997b20e9882de4361
amirhnajafiz <najafizadeh21@gmail.com>
2021-06-12 12:01:27 +0430

    Register page first design added

--- Ring-Frontend/src/components/Login-Register/Register.vue
+++ Ring-Frontend/src/components/Login-Register/Register.vue
@@ -1,26 +1,64 @@
 <template>
-  <div>
-    <h4>Register</h4>
-    <form>
-      <label for="name">Name</label>
-      <input id="name" type="text" v-model="name" required autofocus />
-      <label for="email">E-Mail Address</label>
-      <input id="email" type="email" v-model="email" required />
-      <label for="password">Password</label>
-      <input id="password" type="password" v-model="password" required />
-      <label for="password-confirm">Confirm Password</label>
-      <input
-        id="password-confirm"
-        type="password"
-        v-model="password_confirmation"
-        required
-      />
-      <label for="password-confirm">Is this an administrator account?</label>
-      <select v-model="is_admin">
-        <option value="1">Yes</option>
-        <option value="0">No</option>
-      </select>
-    </form>
+  <div class="h-100 blue-login-background-rvrs">
+    <div class="container h-80">
+      <div class="row align-items-center h-100">
+        <div class="col-3 mx-auto">
+          <div class="text-center">
+            <b-icon
+              icon="gear-fill"
+              animation="spin"
+              style="color: #ffffff"
+              font-scale="4"
+            ></b-icon>
+            <form
+              class="form-signin"
+              id="login-form"
+              @submit.prevent="sendRegisterForm"
+              method="GET"
+            >
+              <input
+                id="name"
+                type="text"
+                v-model="name"
+                class="form-control form-group mt-3"
+                placeholder="Name"
+                required
+                autofocus
+              />
+              <input
+                id="email"
+                type="email"
+                v-model="email"
+                class="form-control form-group mt-3"
+                placeholder="Email"
+                required
+              />
+              <input
+                id="password"
+                type="password"
+                v-model="password"
+                class="form-control form-group mt-3"
+                placeholder="Password"
+                required
+              />
+              <input
+                id="password-confirm"
+                type="password"
+                v-model="password_confirmation"
+                class="form-control form-group mt-3"
+                placeholder="Confirm Password"
+                required
+              />
+              <input
+                class="btn btn-lg btn-primary btn-block btn-signin mt-3 subbtn"
+                type="submit"
+                value="Register"
+              />
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -36,5 +74,17 @@ export default {
       is_admin: null,
     };
   },
+  methods: {
+    sendRegisterForm() {
+      console.log(this.name + " " + this.email);
+    },
+  },
 };
 </script>
+
+
+<style>
+.blue-login-background-rvrs {
+  background-image: linear-gradient(to bottom right, #ffffff, #000066);
+}
+</style>
