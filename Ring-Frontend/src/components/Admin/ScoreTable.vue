524ecd6f579b1929abe8aa6e83d01cb651f68dda
amirhnajafiz <najafizadeh21@gmail.com>
2021-06-13 17:30:21 +0430

    Score table layout done

--- Ring-Frontend/src/components/Admin/ScoreTable.vue
+++ Ring-Frontend/src/components/Admin/ScoreTable.vue
@@ -1,19 +1,32 @@
 <template>
-  <div>
-    <div v-for="team in getTeams" :key="team['t_name']">
-      <label>{{ team["t_name"] }}</label>
-      <b-form inline>
-        <b-form-input v-model="team['new_score']"></b-form-input>
-      </b-form>
-      <b-form-checkbox class="mb-2 mr-sm-2 mb-sm-0" v-model="team['update']"
-        >Change</b-form-checkbox
+  <div class="mt-5 pt-2">
+    <div
+      v-for="team in getTeams"
+      :key="team['t_name']"
+      class="d-flex justify-content-center"
+    >
+      <b-form
+        inline
+        class="d-flex justify-content-around mt-3 mb-3 border w-50 pt-4 pb-4 rounded bg-light border-secondery"
       >
+        <label class="ml-5">{{ team["t_name"] }}</label>
+        <b-form-input
+          v-model="team['new_score']"
+          class="d-inline-block w-25 ml-3 mr-5"
+        ></b-form-input>
+        <b-form-checkbox
+          class="mb-2 mr-5 mb-sm-0 d-inline-block w-25 pr-3 pl-3"
+          v-model="team['update']"
+          size="lg"
+        >تغییر داده شود ؟</b-form-checkbox>
+      </b-form>
     </div>
-    <div class="mt-3">
-      <b-button-group>
-        <b-button variant="success" @click="undo">Undo Changes</b-button>
-        <b-button variant="warning" @click="change">Change</b-button>
-      </b-button-group>
+    <div class="mt-5 text-center pt-3">
+      <b-button variant="primary" @click="undo">بازگشت</b-button>
+      <b-button variant="danger" @click="change">اعمال تغییرات</b-button>
+    </div>
+    <div class="text-center mt-3">
+      <span class="alert-msg">توجه کنید که در صورت اعمال تغییرات نمی توان آن هارا به صورت قبل باز گرداند!</span>
     </div>
   </div>
 </template>
@@ -24,9 +37,9 @@ export default {
   data() {
     return {
       teams: [
-        { t_name: "Alpha", t_score: 20, update: false },
-        { t_name: "Betha", t_score: 10, update: false },
-        { t_name: "Gama", t_score: 50, update: false },
+        { t_name: "Alpha", t_score: 20, update: false, new_score: 0 },
+        { t_name: "Betha", t_score: 10, update: false, new_score: 0 },
+        { t_name: "Gama", t_score: 50, update: false, new_score: 0 },
       ],
     };
   },
@@ -64,3 +77,12 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+.alert-msg {
+  background: #dc3545;
+  color: #ffffff;
+  padding: 3px 10px;
+  border-radius: 25px;
+}
+</style>
