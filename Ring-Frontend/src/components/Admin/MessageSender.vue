703b8608b6894bf051b8882de7bc222bd4aca9ea
amirhnajafiz <najafizadeh21@gmail.com>
2021-06-13 16:28:18 +0430

    Message Sender layout set

--- Ring-Frontend/src/components/Admin/MessageSender.vue
+++ Ring-Frontend/src/components/Admin/MessageSender.vue
@@ -1,24 +1,22 @@
 <template>
-  <div>
+  <div class="m-auto w-50">
     <b-form-group
       label="ارسال پیام به کاربران :"
       label-for="admin-msg"
       description="در نوشتن پیام دقت کنید. بعد از ارسال آن امکان بازگشت  وجود ندارد."
-      class="mb-0"
+      class="mb-3 mt-5"
     >
       <b-form-textarea
         id="admin-msg"
         v-model="adminMessage"
         placeholder="متن خود را وارد کنید"
         lazy-formatter
-        :formatter="formatter"
+        style="height:100px;"
       ></b-form-textarea>
     </b-form-group>
-    <div class="mt-3">
-      <b-button-group>
-        <b-button variant="success" @click="send">ارسال پیام</b-button>
-        <b-button variant="warning" @click="clear">پاک کردن پ‍یام</b-button>
-      </b-button-group>
+    <div class="mt-5">
+        <b-button lg="4" class="ml-5 p-2" variant="success" @click="send">ارسال پیام</b-button>
+        <b-button lg="4" class="mr-5 p-2" variant="danger" @click="clear">پاک کردن پ‍یام</b-button>
     </div>
   </div>
 </template>
@@ -41,3 +39,9 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+label {
+  margin-bottom: 8px;
+}
+</style>
