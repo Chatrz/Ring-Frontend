71cf52e2baad8978a974b05115c51b18ad1ebf01
amirhnajafiz <najafizadeh21@gmail.com>
2021-06-14 10:37:17 +0430

    User dashboard navigation done

--- Ring-Frontend/src/components/Game/Dashboard.vue
+++ Ring-Frontend/src/components/Game/Dashboard.vue
@@ -1,13 +1,25 @@
 <template>
   <div>
-    <b-navbar>
-      <b-navbar-brand href="#" @click="setIndex(0)" :class="linkClass(0)">Ring</b-navbar-brand>
-      <b-nav-item href="#" @click="setIndex(1)" :class="linkClass(1)">Factory</b-nav-item>
-      <b-nav-item href="#" @click="setIndex(2)" :class="linkClass(2)">Land</b-nav-item>
-      <b-nav-item href="#" @click="setIndex(3)" :class="linkClass(3)">Market</b-nav-item>
-      <b-nav-item href="#" @click="setIndex(4)" :class="linkClass(4)">Score Board</b-nav-item>
-      <b-nav-item>{{ "Clock in here "}}</b-nav-item>
-      <b-navbar-toggle target="navbar-toggle-collapse">
+    <b-navbar toggleable>
+      <b-navbar-brand href="#" @click="setIndex(0)" :class="linkClass(0)"
+          >Ring</b-navbar-brand
+        >
+      <b-nav>
+        <b-nav-item href="#" @click="setIndex(1)" :class="linkClass(1)"
+          >Factory</b-nav-item
+        >
+        <b-nav-item href="#" @click="setIndex(2)" :class="linkClass(2)"
+          >Land</b-nav-item
+        >
+        <b-nav-item href="#" @click="setIndex(3)" :class="linkClass(3)"
+          >Market</b-nav-item
+        >
+        <b-nav-item href="#" @click="setIndex(4)" :class="linkClass(4)"
+          >Score Board</b-nav-item
+        >
+        <b-nav-item>{{ "Clock in here " }}</b-nav-item>
+      </b-nav>
+      <b-navbar-toggle target="navbar-toggle-collapse" style="margin-left: 15px;">
         <template #default="{ expanded }">
           <b-icon v-if="expanded" icon="chevron-bar-up"></b-icon>
           <b-icon v-else icon="chevron-bar-down"></b-icon>
@@ -15,8 +27,10 @@
       </b-navbar-toggle>
       <b-collapse id="navbar-toggle-collapse" is-nav>
         <b-navbar-nav class="ml-auto">
-          <b-nav-item href="#">{{ username + " | " + email }}</b-nav-item>
-          <b-nav-item href="#">Logout</b-nav-item>
+          <b-nav-item style="text-align:left; padding-left: 10px;" href="#">{{ username + " | " + email }}</b-nav-item>
+          <b-nav-item style="text-align:left; padding-left: 10px;" href="#">
+            <button class="btn btn-danger text-light" @click="logout">Logout</button>
+          </b-nav-item>
         </b-navbar-nav>
       </b-collapse>
     </b-navbar>
@@ -36,7 +50,7 @@
 import Factory from "./Factory.vue";
 import Land from "./Land.vue";
 import Market from "./Market.vue";
-import Scoreboard from './Scoreboard.vue';
+import Scoreboard from "./Scoreboard.vue";
 export default {
   components: { Factory, Land, Market, Scoreboard },
   name: "Dashboard",
@@ -57,7 +71,10 @@ export default {
       } else {
         return ["bg-light", "text-dark"];
       }
-    }
+    },
+    logout() {
+      this.$router.push("/");
+    },
   },
 };
 </script>
