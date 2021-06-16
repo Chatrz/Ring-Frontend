0ba4c4f687c5ee149a2b4f6fd1242e3a41ddef55
amirhnajafiz <najafizadeh21@gmail.com>
2021-06-16 11:20:02 +0430

    Add land slider

--- Ring-Frontend/src/components/Game/Land.vue
+++ Ring-Frontend/src/components/Game/Land.vue
@@ -1,6 +1,51 @@
 <template>
-  <div>
-    {{ "This is " + name + " page" }}
+  <div class="text-center">
+    <h2 class="mt-4 mb-5">{{ "Choose your land :" }}</h2>
+    <b-carousel
+      id="landSlider"
+      v-model="slide"
+      class="mb-5 m-auto w-75"
+      controls
+      indicators
+      fade
+      background="#ababab"
+      img-width="512"
+      img-height="240"
+      style="text-shadow: 1px 1px 2px #333"
+    >
+      <b-carousel-slide
+        caption="First slide"
+        text="Nulla vitae elit libero, a pharetra augue mollis interdum."
+        img-src="https://picsum.photos/1024/480/?image=52"
+      ></b-carousel-slide>
+      <b-carousel-slide img-src="https://picsum.photos/1024/480/?image=54">
+        <h1>Hello world!</h1>
+      </b-carousel-slide>
+
+      <b-carousel-slide
+        img-src="https://picsum.photos/1024/480/?image=58"
+      ></b-carousel-slide>
+
+      <b-carousel-slide>
+        <template #img>
+          <img
+            class="d-block img-fluid w-100"
+            width="1024"
+            height="480"
+            src="https://picsum.photos/1024/480/?image=55"
+            alt="image slot"
+          />
+        </template>
+      </b-carousel-slide>
+
+      <b-carousel-slide caption="Blank Image" img-blank img-alt="Blank image">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+          eros felis, tincidunt a tincidunt eget, convallis vel est. Ut
+          pellentesque ut lacus vel interdum.
+        </p>
+      </b-carousel-slide>
+    </b-carousel>
   </div>
 </template>
 
@@ -8,13 +53,10 @@
 export default {
   name: "Land",
   data() {
-    return {};
+    return {
+      slide: 0,
+    };
   },
-  computed: {
-    name() {
-      return "Land";
-    }
-  }
 };
 </script>
 
