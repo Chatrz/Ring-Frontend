d1965f30dcc90453e3ed9478f4f124e9c32cf40b
amirhnajafiz <najafizadeh21@gmail.com>
2021-06-18 10:39:57 +0430

    Warehouse finished

--- Ring-Frontend/src/components/Game/Factory/ProductionLine.vue
+++ Ring-Frontend/src/components/Game/Factory/ProductionLine.vue
@@ -1,5 +1,20 @@
 <template>
   <div>
+    <b-icon
+      icon="diagram3-fill"
+      font-scale="5"
+      style="
+        color: #f07c00ff;
+        width: 800px;
+        height: 800px;
+        overflow: hidden;
+        z-index: -1;
+        position: fixed;
+        right: 50px;
+        top: 50px;
+        text-overflow: ellipsis;
+      "
+    ></b-icon>
     <h2 class="text-center mt-4 mb-4">{{ "Status: On / Off" }}</h2>
     <machine />
     <div class="text-center mt-4 mb-4">
--- Ring-Frontend/src/components/Game/Factory/Warehouse.vue
+++ Ring-Frontend/src/components/Game/Factory/Warehouse.vue
@@ -1,37 +1,104 @@
 <template>
   <div>
+    <b-icon
+      icon="box-seam"
+      font-scale="5"
+      style="
+        color: #f07c00ff;
+        width: 400px;
+        height: 400px;
+        overflow: hidden;
+        z-index: -1;
+        position: fixed;
+        left: 0px;
+        top: 50px;
+        text-overflow: ellipsis;
+      "
+    ></b-icon>
+    <b-icon
+      icon="box"
+      font-scale="5"
+      style="
+        color: #f07c00ff;
+        width: 500px;
+        height: 500px;
+        overflow: hidden;
+        z-index: -1;
+        position: fixed;
+        right: 0px;
+        bottom: 50px;
+        text-overflow: ellipsis;
+      "
+    ></b-icon>
+    <b-icon
+      icon="box-seam"
+      font-scale="5"
+      style="
+        color: #adadad;
+        width: 300px;
+        height: 300px;
+        overflow: hidden;
+        z-index: -1;
+        position: fixed;
+        left: 10px;
+        bottom: 80px;
+        text-overflow: ellipsis;
+      "
+    ></b-icon>
     <h1 class="text-center mt-5 mb-5">
       {{ "انبار" }}
     </h1>
-    <div class="m-auto w-50 mb-5 mt-3 rounded bg-light" style="padding: 20px; height: 350px;">
+    <div
+      class="m-auto w-50 mb-5 mt-3 rounded bg-light"
+      style="padding: 20px; height: 350px"
+    >
       <h3 class="mb-5">
         {{ "وضعیت فعلی انبار شما :" }}
       </h3>
       <div class="m-auto mb-2 w-50">
-        <div class="bg-success text-light rounded w-100" style="padding:15px;">{{ "ظرفیت انبار :  " + getWareInfo.capacity }}</div>
+        <div class="bg-success text-light rounded w-100" style="padding: 15px">
+          {{ "ظرفیت انبار :  " + getWareInfo.capacity }}
+        </div>
       </div>
       <div class="m-auto mb-2 w-50">
-        <div class="bg-success text-light rounded w-100" style="padding:15px;">{{ "قیمت فعلی انبار :  " + getWareInfo.value + " $" }}</div>
+        <div class="bg-success text-light rounded w-100" style="padding: 15px">
+          {{ "قیمت فعلی انبار :  " + getWareInfo.value + " $" }}
+        </div>
       </div>
-      <div class=" m-auto w-50">
-        <div class="bg-info text-light rounded w-100" style="padding:15px;">{{ "کالاهای موجود در انبار :  " + current_capacity }}</div>
+      <div class="m-auto w-50">
+        <div class="bg-info text-light rounded w-100" style="padding: 15px">
+          {{ "کالاهای موجود در انبار :  " + current_capacity }}
+        </div>
       </div>
       <div v-show="current_capacity == getWareInfo.capacity" class="mt-3 mb-2">
-        {{ "ظرفیت انبار شما تکمیل شده است. در صورت اقدام نکردن برای ارتقای انبار یا خالی نکردن انبار کالاهای شما توسط ادمین با قیمت عادی ( بدون سود ) خریداری خواهند شد." }}
+        {{
+          "ظرفیت انبار شما تکمیل شده است. در صورت اقدام نکردن برای ارتقای انبار یا خالی نکردن انبار کالاهای شما توسط ادمین با قیمت عادی ( بدون سود ) خریداری خواهند شد."
+        }}
       </div>
     </div>
     <div class="m-auto w-50 mt-5 rounded bg-light" style="padding: 20px">
-      <h4>
+      <h4 class="mb-3">
         {{ "ارتقای انبار :" }}
       </h4>
-      <div v-for="plan in plans" :key="plan.id" style="height:50px;">
+      <div v-for="plan in plans" :key="plan.id" style="height: 50px">
         <span>
           {{ "ظرفیت :  " }}
-          <b-badge variant="danger" style="color: #ffffff; background: #dc3545; padding: 6px;">
+          <b-badge
+            variant="danger"
+            style="color: #ffffff; background: #dc3545; padding: 6px"
+          >
             {{ plan.capacity }}
           </b-badge>
           {{ "قیمت :  " }}
-          <b-badge variant="danger" style="color: #ffffff; background: #dc3545; padding: 6px; margin-right: 5px;">
+          <b-badge
+            variant="danger"
+            style="
+              color: #ffffff;
+              background: #dc3545;
+              padding: 6px;
+              margin-right: 5px;
+            "
+          >
             {{ plan.value }}
           </b-badge>
         </span>
@@ -45,7 +112,7 @@
             { disabled: current_plan >= plan.id - 1 },
           ]"
           @click="increaseCapacity(plan.id - 1)"
-          style="float: left;"
+          style="float: left"
         >
           {{ "به همین وضع ارتقاع بده." }}
         </button>
