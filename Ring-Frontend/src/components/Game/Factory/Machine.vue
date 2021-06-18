f26cefa47461b5d8b7e79376ace4cfa9c700c37d
amirhnajafiz <najafizadeh21@gmail.com>
2021-06-18 11:24:56 +0430

    Production line animation

--- Ring-Frontend/src/components/Game/Factory/Machine.vue
+++ Ring-Frontend/src/components/Game/Factory/Machine.vue
@@ -1,16 +1,16 @@
 <template>
   <div class="d-flex justify-content-center mt-5">
     <div class="d-flex align-items-center">
-      <div class="gray-box"></div>
+      <div class="gray-box" v-show="index == 0"></div>
       <b-icon
         animation="cylon"
         icon="arrow-left-square-fill"
-        v-if="active"
+        v-if="active && index == 0"
       ></b-icon>
       <b-icon icon="x-circle" v-else></b-icon>
     </div>
     <div
-      class="bg-primary rounded d-flex justify-content-evenly align-items-center"
+      class="bg-danger rounded d-flex justify-content-evenly align-items-center"
       style="width: 400px; height: 120px; margin: 0px 20px"
     >
       <b-icon icon="box" animation="throb" font-scale="2"></b-icon>
@@ -25,14 +25,36 @@
       <b-icon icon="gear-wide" animation="spin" font-scale="2"></b-icon>
     </div>
     <div class="d-flex align-items-center">
-      <b-icon animation="cylon" icon="arrow-left" v-if="active"></b-icon>
+      <b-icon animation="cylon" icon="arrow-left" v-if="active && index == 1"></b-icon>
       <b-icon icon="x-circle" v-else></b-icon>
-      <div class="gray-box rounded"></div>
-      <b-icon animation="cylon" icon="arrow-left" v-if="active"></b-icon>
+      <div class="gray-box rounded" v-show="index == 1"></div>
+      <b-icon animation="cylon" icon="arrow-left" v-if="active && index == 1"></b-icon>
       <b-icon icon="x-circle" v-else></b-icon>
     </div>
     <div
-      class="bg-danger rounded d-flex justify-content-evenly align-items-center"
+      class="bg-primary rounded d-flex justify-content-evenly align-items-center"
+      style="width: 400px; height: 120px; margin: 0px 20px"
+    >
+      <b-icon icon="gear-fill" animation="spin-reverse" font-scale="2"></b-icon>
+      <b-icon
+        icon="gear-wide-connected"
+        animation="spin"
+        font-scale="2"
+      ></b-icon>
+      <b-icon icon="wrench" animation="spin-pulse" font-scale="2"></b-icon>
+      <b-icon icon="gear-fill" animation="spin" font-scale="2"></b-icon>
+      <b-icon icon="brush" animation="cylon" font-scale="2"></b-icon>
+      <b-icon icon="arrow-repeat" animation="spin" font-scale="2"></b-icon>
+    </div>
+    <div class="d-flex align-items-center">
+      <b-icon animation="cylon" icon="arrow-left" v-if="active && index == 2"></b-icon>
+      <b-icon icon="x-circle" v-else></b-icon>
+      <div class="gray-box rounded-circle" v-show="index == 2"></div>
+      <b-icon animation="cylon" icon="arrow-left" v-if="active && index == 2"></b-icon>
+      <b-icon icon="x-circle" v-else></b-icon>
+    </div>
+    <div
+      class="bg-success rounded d-flex justify-content-evenly align-items-center"
       style="width: 400px; height: 120px; margin: 0px 20px"
     >
       <b-icon icon="gear-fill" animation="spin-reverse" font-scale="2"></b-icon>
@@ -50,10 +72,10 @@
       <b-icon
         animation="cylon"
         icon="arrow-left-square-fill"
-        v-if="active"
+        v-if="active && index == 3"
       ></b-icon>
       <b-icon icon="x-circle" v-else></b-icon>
-      <div class="gray-box rounded-circle"></div>
+      <div class="gray-box rounded-circle" v-show="index == 3"></div>
     </div>
   </div>
 </template>
@@ -64,8 +86,22 @@ export default {
   data() {
     return {
       active: true,
+      index: 0,
+      timer: 2000
     };
   },
+  methods: {
+    nextLevel() {
+      this.index++;
+      this.index %= 4;
+      setTimeout(() => {
+        this.nextLevel();
+      }, this.timer);
+    }
+  },
+  mounted() {
+    this.nextLevel();
+  }
 };
 </script>
 
