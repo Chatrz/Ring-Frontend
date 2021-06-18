38c892202ce62bcc58a35589bd198729315c3d4b
amirhnajafiz <najafizadeh21@gmail.com>
2021-06-18 09:41:10 +0430

    Product line updated

--- Ring-Frontend/src/components/Game/Factory/ProductionLine.vue
+++ Ring-Frontend/src/components/Game/Factory/ProductionLine.vue
@@ -2,19 +2,23 @@
   <div>
     <h2 class="text-center mt-4 mb-4">{{ "Status: On / Off" }}</h2>
     <machine />
-    <div class="text-center mt-4 mb-4"> 
-      {{ "Product information" }}
+    <div class="text-center mt-4 mb-4">
+      <div v-for="item in product" :key="'$' + item">
+        {{ item }}
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-import Machine from './Machine.vue';
+import Machine from "./Machine.vue";
 export default {
   components: { Machine },
   name: "ProductionLine",
   data() {
-    return {};
+    return {
+      product: { id: 101, type: "Normal", waterSaver: true },
+    };
   },
 };
 </script>
