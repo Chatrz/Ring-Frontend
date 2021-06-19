3915e751758bd290959d59be2393bedda23d1bfb
amirhnajafiz <najafizadeh21@gmail.com>
2021-06-19 10:41:12 +0430

    Sales Department basics added

--- Ring-Frontend/src/components/Game/Market/PurchaseSection.vue
+++ Ring-Frontend/src/components/Game/Market/PurchaseSection.vue
@@ -19,26 +19,44 @@
       {{ "بخش خرید کالا" }}
     </h2>
     <div>
-      <div v-for="item in items" :key="item.key" class="d-flex justify-content-between align-self-center bg-success text-light rounded m-auto w-75 p-4 mb-1">
-        <div style="flex-grow: 0; flex-basis: 0; padding-left: 5px;">
-          <div class="rounded-circle bg-secondary" style="width:30px; height:30px;"></div>
+      <div
+        v-for="item in items"
+        :key="item.key"
+        class="d-flex justify-content-between align-self-center bg-success text-light rounded m-auto w-75 p-4 mb-1"
+      >
+        <div style="flex-grow: 0; flex-basis: 0; padding-left: 5px" class="m-auto">
+          <div
+            class="rounded-circle bg-secondary"
+            style="width: 30px; height: 30px"
+          ></div>
         </div>
-        <div style="flex-grow: 1; flex-basis: 0;"> 
+        <div style="flex-grow: 1; flex-basis: 0" class="m-auto">
           {{ item.label }}
         </div>
-        <div style="flex-grow: 1; flex-basis: 0;" class="text-center">
-          <span style="font-size: 18px;">
-            {{ item.value + " $"}}
+        <div style="flex-grow: 1; flex-basis: 0" class="text-center m-auto">
+          <span style="font-size: 18px" class="bg-info text-light p-2 rounded">
+            {{ "قیمت پایه: " + item.value + " $" }}
           </span>
         </div>
-        <div class="d-flex justify-content-evenly" style="flex-grow: 1; flex-basis: 0;">
-          <button type="button" class="btn btn-danger" @click="decrease(item.key, item.value)">
+        <div
+          class="d-flex justify-content-evenly m-auto"
+          style="flex-grow: 1; flex-basis: 0"
+        >
+          <button
+            type="button"
+            class="btn btn-danger"
+            @click="decrease(item.key, item.value)"
+          >
             <b-icon icon="caret-right-fill" scale="1"></b-icon>
           </button>
-          <span style="font-size: 18px;">
+          <span style="font-size: 18px">
             {{ getItemNumber(item.key) }}
           </span>
-          <button type="button" class="btn btn-danger" @click="increase(item.key, item.value)">
+          <button
+            type="button"
+            class="btn btn-danger"
+            @click="increase(item.key, item.value)"
+          >
             <b-icon icon="caret-left-fill" scale="1"></b-icon>
           </button>
         </div>
@@ -46,7 +64,7 @@
     </div>
     <div class="text-center mt-5">
       <h3 class="mt-5 mb-5">
-        {{ "مبلغ کل خرید " + total + " $"}}
+        {{ "مبلغ کل خرید " + total + " $" }}
       </h3>
       <button type="button" class="btn btn-primary" @click="confirmPuurchase">
         {{ "ثبت خرید ها" }}
@@ -68,11 +86,11 @@ export default {
         { key: "powerfull", label: "بدنه محکم تر", value: 15 },
       ],
       purchase: [],
-      total: 0
+      total: 0,
     };
   },
   created() {
-    this.items.forEach(element => {
+    this.items.forEach((element) => {
       this.purchase[element.key] = 0;
     });
   },
@@ -82,8 +100,7 @@ export default {
       this.total += value;
     },
     decrease(key, value) {
-      if (this.purchase[key] > 0)
-      {
+      if (this.purchase[key] > 0) {
         this.purchase[key]--;
         this.total -= value;
       }
@@ -96,11 +113,11 @@ export default {
       return this.purchase[key];
     },
     resetFactor() {
-      this.items.forEach(element => {
+      this.items.forEach((element) => {
         this.purchase[element.key] = 0;
         this.total = 0;
       });
-    }
+    },
   },
 };
 </script>
--- Ring-Frontend/src/components/Game/Market/SalesDepartment.vue
+++ Ring-Frontend/src/components/Game/Market/SalesDepartment.vue
@@ -1,6 +1,36 @@
 <template>
   <div>
-      {{ "Sales Department" }}
+    <h2 class="text-center mt-5 mb-1">
+      {{ "بخش فروش محصولات" }}
+    </h2>
+    <div class="text-center mb-5 pb-3">
+      {{ "خرید کالاهای شما توسط ربات مسابقه صورت می گیرد. توجه کنید که مقدار سود شما در بازه های زمانی مختلف مسابقه ممکن است متفاوت باشد." }}
+    </div>
+    <h3 class="text-center mt-2 mb-1">
+      {{ "محصولات فعلی موجود در انبار:" }}
+    </h3>
+    <div>
+      <div v-for="item in items" :key="item.key" class="d-flex justify-content-between align-self-center bg-success text-light rounded m-auto w-75 p-4 mb-1">
+        <div style="flex-grow: 1; flex-basis: 0" class="m-auto">
+          {{ item.label }}
+        </div>
+        <div style="flex-grow: 1; flex-basis: 0" class="text-center w-50 m-auto">
+          <span class="bg-info text-light p-2 rounded"> 
+            {{ "هزینه تمام شده: " + item.value + " $" }}
+          </span>
+        </div>
+        <div style="flex-grow: 1; flex-basis: 0" class="text-center w-50 m-auto">
+          <span class="bg-danger text-light p-2 rounded">  
+          {{ "مانده: " + item.number }}
+          </span>
+        </div>
+        <div style="flex-grow: 1; flex-basis: 0" class="text-center">
+          <button type="button" class="btn btn-primary" @click="sellProduct(item.key)">
+            {{ "بفروش" }}
+          </button>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -8,8 +38,26 @@
 export default {
   name: "SalesDepartment",
   data() {
-    return {};
+    return {
+      items: [
+        { key: "watersaver", label: "لباس شویی کم مصرف", value: 100, number: 10 },
+        { key: "normal", label: "لباس شویی عادی", value: 50, number: 20 },
+        { key: "powerfull", label: "لباس شویی مقاوم", value: 120, number: 0 },
+      ],
+    };
   },
+  methods: {
+    sellProduct(key) {
+      this.items.forEach(element => {
+        if (element.key == key) {
+          if (element.number > 0)
+            element.number--;
+          else
+            alert("You don't have any of this item to sell.")
+        }
+      });
+    }
+  }
 };
 </script>
 
